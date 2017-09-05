1938e4531bfb7c66a0f5537d9e1a0170b93e2f0c
Sudheera Daggumalli <sudheeradaggumalli@gmail.com>
2017-09-05 09:26:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,5 +7,15 @@ def display_board(board)
     puts "-----------"
     puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
+board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+display_board(board)
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+display_board(board)
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 display_board(board)
